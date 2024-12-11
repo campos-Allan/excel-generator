959e47dd115d26693e4c64a0e3bb9e7d69ac871d
Allan Campos <allan.campos97@gmail.com>
2024-12-10 21:23:26 -0300

    Update Excel generator.py

--- Excel generator.py
+++ Excel generator.py
@@ -5,23 +5,25 @@ then saving with proper name
 
 from datetime import datetime
 from datetime import timedelta
+import os
 import win32com.client
 
 trinta = [4, 6, 9, 11]
 trinta_um = [1, 3, 5, 7, 8, 10, 12]
-
-if datetime.now().month in trinta:
-    MODELO = 'path/MODELO 30.xlsx'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#selecting spreadsheet model based on the month
+if datetime.now().month in trinta:  #NOT THE REAL SPREASHEETS USED FOR THIS, ONLY EXAMPLES
+    MODELO = dir_path+'\\MODELO 30.xlsx'
 elif datetime.now().month in trinta_um:
-    MODELO = 'path/MODELO 31.xlsx'
+    MODELO = dir_path+'\\MODELO 31.xlsx'
 else:
-    MODELO = 'path/MODELO 29.xlsx'
+    MODELO = dir_path+'\\MODELO 29.xlsx'
 try:
     EXCEL = win32com.client.Dispatch("EXCEL.Application")
     EXCEL.Visible = False
     PLANILHA = EXCEL.Workbooks.Open(MODELO)
-    SHEET = PLANILHA.WorkSHEETs('sheet name')
-    SHEET.Cells(2, 2).Value = datetime.now()
+    SHEET = PLANILHA.Worksheets('Main')
+    SHEET.Cells(2, 2).Value = datetime.now() #putting values in cells that influence queries
     SHEET.Cells(1, 2).Value = (datetime.now()+timedelta(days=3))
     ano = datetime.now().strftime('%y')
     mes = datetime.now().strftime('%m')
@@ -31,7 +33,7 @@ try:
     dia_aprest = (datetime.now()+timedelta(days=3)).strftime('%d')
     mes_aprest = (datetime.now()+timedelta(days=3)).strftime('%m')
     ano_aprest = (datetime.now()+timedelta(days=3)).strftime('%y')
-    PLANILHA.SaveAs(Filename=f"path\\Acompanhamento Semanal de Movimentações_{
+    PLANILHA.SaveAs(Filename=f"{dir_path}\\Acompanhamento Semanal_{
                     dia_aprest}-{mes_aprest}-{ano_aprest}.xlsx")
     PLANILHA.Close()
     EXCEL.Quit()
